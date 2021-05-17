618a2b81381dafd7d336e942c3c6a6f9e35ca1a9
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-05-17 19:02:37 +0500

    Move menu description text into text container

--- src/menu.js
+++ src/menu.js
@@ -12,32 +12,38 @@ function createMenu() {
     const menuCards = createE("div", false, "menu-cards");
     const card1 = createE("div", false, "card");
     const card1Img = createImg("../src/images/menu01.jpg");
+    const card1Text = createE("div", false, "card-text");
     const card1H5 = createE("h5", "Spicy beef salad");
     const card1P = createE("p");
     card1P.innerHTML = "Where to start your meal if not with a hearty salad, perfect for whetting your appetite. It is based on tender beef cooked over low heat for a long time, as well as crispy fresh vegetables and a spicy Pan-Asian sauce of chili and lime. It tastes just fiery, while any guest will enjoy the dish and even require additives. It is not surprising that this is an absolute favorite among salads due to the extraordinary juiciness of the beef and the 'brightness' of the sauce."
     card1.appendChild(card1Img);
-    card1.appendChild(card1H5);
-    card1.appendChild(card1P);
+    card1Text.appendChild(card1H5);
+    card1Text.appendChild(card1P);
+    card1.appendChild(card1Text);
     menuCards.appendChild(card1);
     
     const card2 = createE("div", false, "card");
     const card2Img = createImg("../src/images/menu02.jpg");
+    const card2Text = createE("div", false, "card-text");
     const card2H5 = createE("h5", "Nasi Goreng");
     const card2P = createE("p");
     card2P.innerHTML = "Many have heard about one of the signature dishes of Pan-Asian cuisine, which is very popular all over the world, have tried it in Southeast Asia and would like to re-experience the unusual taste at home. It has many varieties, and Rusnadin Mukhtar offers his own proprietary approach. First, vegetables and eggs are fried in a traditional wok, then chili paste, soy sauce and chicken with shrimps are added. When everything is browned, put the pre-cooked rice in the pan and mix its contents. Nasi goreng at City 21 is served with shrimp kebabs, grilled chicken, crackers and vegetable salad. The traditional dish is spicy, but at the request of the guests, it can be muted or removed altogether."
     card2.appendChild(card2Img);
-    card2.appendChild(card2H5);
-    card2.appendChild(card2P);
+    card2Text.appendChild(card2H5);
+    card2Text.appendChild(card2P);
+    card2.appendChild(card2Text);
     menuCards.appendChild(card2);
 
     const card3 = createE("div", false, "card");
     const card3Img = createImg("../src/images/menu03.jpg");
+    const card3Text = createE("div", false, "card-text");
     const card3H5 = createE("h5", "Shrimp with wasabi");
     const card3P = createE("p");
     card3P.innerHTML = "Guests of City 21 will enjoy a real treat - juicy and crunchy tiger prawns, fried in hot oil and seasoned with a hot wasabi sauce prepared according to the chef's original recipe. What gives rise to such a combination is a surprisingly contrasting taste, built on a mixture of milky sweet and spicy. A hot appetizer is served with Japanese radish - daikon, chukka salad and red roe of flying fish - tobiko. "
     card3.appendChild(card3Img);
-    card3.appendChild(card3H5);
-    card3.appendChild(card3P);
+    card3Text.appendChild(card3H5);
+    card3Text.appendChild(card3P);
+    card3.appendChild(card3Text);
     menuCards.appendChild(card3);
 
     menu.appendChild(menuCards)
